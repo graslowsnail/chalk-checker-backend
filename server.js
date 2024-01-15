e3bc6d341162cdfceb5ed1786dbd0060d169aea6
graslowsnail <graslowsnail@gmail.com>
2024-01-15 13:22:00 -0600

    added mongoose package and established connection to a mongodb database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // npm start to run with nodemon
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 // routes 
 const playerRoutes = require('./routes/prizePicksRouts.js');
@@ -11,9 +12,21 @@ const app = express();
 // EXAMPLE: this is how a api call will be made to a api 
 app.use('/api', playerRoutes);
 
-// this logs the mongoDb queries being executed
-// mongoose.set('debug', true);
 
+// to stop deprecation warning in console
+mongoose.set('strictQuery', false);
+
+// connect to mongoDb 
+mongoose.connect(
+    process.env.MONGO_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+    }
+);
+
+// this logs the mongoDb queries being executed
+mongoose.set('debug', true);
 
 app.listen(process.env.PORT, () => {
     console.log(' connected to Bet Checker backend🌎');
